e11089fa355742ad9e2973fe25ee4b8286176dd0
dialloflatno <brahimdiallo23@gmail.com>
2021-10-17 03:52:42 -0400

    commit tree c428462292875a00fc03219d77e7f2cf587e37f2

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function divide(x=2000,y=100){
+                return(x/y );
+}
+
+console.log(divide());
+
+const square = (x=2) =>(x*x)
+
+console.log(square());
+
+const add = (a= 3,b=4) => { return a+b};
+
+console.log(add())
